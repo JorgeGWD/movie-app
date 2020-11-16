f167424988665778aca4f2a4aa7119a454a84fe1
JorgeGWD <jorge.rodriguez.r@gmail.com>
2020-11-16 13:29:09 -0500

    Create Search component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React from 'react'
+import Search from './components/Search'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Movie App</h1>
-      </header>
-    </div>
-  );
+
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>Movie App</h1>
+            </header>
+            <main>
+                <Search />
+            </main>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/Search.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Search() {
+    return (
+        <section className="search-box">
+            <input type="text" className="search-bar" placeholder="Search" />
+        </section>
+    )
+}
+
+export default Search
